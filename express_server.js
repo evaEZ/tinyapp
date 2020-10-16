979c6a2b49e3b16c2b89a8098fcaca3a97272693
evaEZ <eva.jingzhao@gmail.com>
2020-10-16 04:58:19 +0000

    remove some dead code

--- express_server.js
+++ express_server.js
@@ -18,12 +18,6 @@ app.use(cookieSession({
   name: 'session',
   keys: ['testtinyapp', 'tinyapptest']
 }));
-/** 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-*/
 
 const urlDatabase = {
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
